7e8c57ea61c3a6b4617986c073e054f223270522
JiangHai <hanayo@kayotin.cn>
2023-04-11 15:02:33 +0800

    update 0411

--- summary_files.py
+++ summary_files.py
@@ -20,12 +20,12 @@ def sum_csv(files, start_row):
     sum_col, sum_row = 1, 1
 
     for csv_file in files:
-        row_num = 0
+        row_num = 1
         with open(csv_file, "r", encoding="utf-8") as file:
             row = file.readline()
             while row:
                 row_list = row.split(",")
-                if row_num > start_row:
+                if row_num > start_row - 1:
                     for col in range(len(row_list)):
                         sum_sheet.cell(sum_row, sum_col, row_list[col])
                         sum_col += 1
@@ -46,7 +46,7 @@ def sum_xls(files_list, start_num):
     for xls_file in files_list:
         wb = xlrd.open_workbook(xls_file)
         sheet = wb.sheet_by_index(0)
-        for row in range(start_num, sheet.nrows):
+        for row in range(start_num - 1, sheet.nrows):
             for col in range(sheet.ncols):
                 cell_value = sheet.cell_value(row, col)
                 sum_sheet.cell(sum_row, sum_col, cell_value)
@@ -73,12 +73,14 @@ def sum_xlsx(x_list, x_s_num):
                 sum_col += 1
             sum_row += 1
             sum_col = 1  # 读完一行，列重新从1开始
+            if sum_row % 1000 == 0:
+                print(f"已写入{sum_row}行数据--->\n")
 
     sum_wb.save("./output/summary_excel.xlsx")
     print(f"合并完毕，共合并了{sum_row}行数据--->")
 
 
-def sum_html(files):
+def sum_html(files, start_num):
     st_time = time.time()
     sum_wb = openpyxl.Workbook()  # type: Workbook
     sum_sheet = sum_wb.worksheets[0]  # type: Worksheet
@@ -115,7 +117,8 @@ def get_files_from_folder(input_path):
 file_type = {
     "1": sum_csv,
     "2": sum_xls,
-    "3": sum_xlsx
+    "3": sum_xlsx,
+    "4": sum_html
 }
 
 
@@ -131,8 +134,16 @@ def main():
     save_path = Path("./output")
     if not save_path.exists():
         Path.mkdir(save_path)
-    # start_num = int(input("请输入数据开始的列："))
-    sum_csv(files_list, 2)
+    print("""
+    请选择您要进行哪种操作：
+    1：合并csv\n
+    2：合并xls\n
+    3：合并xlsx\n
+    4：合并html\n
+    """)
+    op_type = input("请输入数字：")
+    start_num = int(input("请输入数据开始的列："))
+    file_type[op_type](files_list, start_num)
 
 
 if __name__ == '__main__':
